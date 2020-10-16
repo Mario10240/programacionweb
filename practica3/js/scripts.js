c506cbae6d7c593f5dfadccf381e8e394929e13d
Mario Valdez <mario10240@live.com>
2020-10-16 01:02:47 -0700

    Practica 3 añadida

---
+++ practica3/js/scripts.js
@@ -0,0 +1,25 @@
+$(window).on("load", function(){
+  // Handler when all assets (including images) are loaded.
+
+  // Find carousel.
+  // document.getElementById("carouselExampleIndicators").carousel({
+  //     interval: 100
+  // });
+  
+  $('#mycarousel').carousel({ interval: 2000 });
+});
+
+$('.collapse').collapse()
+
+$('#myCollapsible').collapse({
+  toggle: false
+})
+
+$('#myCollapsible').on('hidden.bs.collapse', function () {
+  // do something…
+})
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
+
